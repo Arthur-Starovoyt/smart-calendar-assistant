7e04d6bcd69c885e33f447911012b2f20cdd3123
Arthur_Starovoyt <89992433888@mail.ru>
2026-06-21 15:03:53 +0300

    chore: initialize spring boot project

---
+++ src/main/java/ru/starovoyt/smart_calendar_assistant/SmartCalendarAssistantApplication.java
@@ -0,0 +1,13 @@
+package ru.starovoyt.smart_calendar_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCalendarAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCalendarAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/starovoyt/smart_calendar_assistant/SmartCalendarAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.starovoyt.smart_calendar_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCalendarAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
